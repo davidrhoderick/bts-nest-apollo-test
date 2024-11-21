4a50e2b516aef57c7a05b9b9dbc680fab7694f7a
David Rhoderick <david.e.rhoderick@gmail.com>
2024-11-21 17:54:26 +0100

    Creating OfficialIds form using mock data without types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import LossHistoryEntries from "./components/loss-history-entries";
+import OfficialIds from "./components/official-ids";
 
 function App() {
-  return <LossHistoryEntries />;
+  return (
+    <>
+      <LossHistoryEntries />
+      <OfficialIds />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/official-ids.tsx
@@ -0,0 +1,57 @@
+import { useForm } from "react-hook-form";
+
+export default function OfficialIds() {
+  // TODO get this from useRouter
+  const transactionId = "123456";
+
+  // TODO get this from useQuery
+  const states = [
+    {
+      state: "HI",
+      displayName: "Hawaii",
+      officialIds: [
+        {
+          label: "State Unemployment ID",
+          code: "STUN",
+          id: "12315123",
+        },
+      ],
+    },
+  ];
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: { transactionId, states },
+  });
+
+  const onSubmit = (formData) => {
+    console.log("transactionId", formData.transactionId);
+    console.log("officialIds", formData.states);
+
+    // TODO submit with useMutation
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="hidden" {...register("transactionId")} />
+      {states.map(({ displayName, state, officialIds }, stateIndex) => (
+        <div key={state}>
+          <h1>{displayName}</h1>
+
+          {officialIds.map(({ label, code }, codeIndex) => (
+            <div key={`${state}-${code}`}>
+              <label htmlFor={`${state}-${code}`}>{label}</label>
+
+              <input
+                {...register(
+                  `states.${stateIndex}.officialIds.${codeIndex}.id`
+                )}
+              />
+            </div>
+          ))}
+        </div>
+      ))}
+
+      <button type="submit">Continue</button>
+    </form>
+  );
+}
