8ab9204ea0802e0191ec1c63a78a39ff3dad33f6
davidrhoderick <david.e.rhoderick@gmail.com>
2024-11-21 14:33:44 +0100

    Making loss history entries it's own component

---
+++ frontend/src/components/loss-history-entries.tsx
@@ -0,0 +1,64 @@
+import { useMutation, useQuery } from "@apollo/client";
+import {
+  IListLossHistoryEntriesArgs,
+  ILossHistoryEntries,
+  IUpdateLossHistoryEntriesInputVariables,
+} from "@bts-api-tests/types";
+import { useCallback } from "react";
+import {
+  LIST_LOSS_HISTORY_ENTRIES,
+  UPDATE_LOSS_HISTORY_ENTRIES,
+} from "../gql/loss-history-entries";
+
+export default function LossHistoryEntries() {
+  const { data, loading, error } = useQuery<
+    ILossHistoryEntries,
+    IListLossHistoryEntriesArgs
+  >(LIST_LOSS_HISTORY_ENTRIES, { variables: { transactionId: "12312512" } });
+
+  const [updateLossHistoryEntries, { loading: mutationLoading }] = useMutation<
+    ILossHistoryEntries,
+    IUpdateLossHistoryEntriesInputVariables
+  >(UPDATE_LOSS_HISTORY_ENTRIES);
+
+  const mutateHandler = useCallback(() => {
+    updateLossHistoryEntries({
+      variables: {
+        updateLossHistoryEntriesInput: {
+          transactionId: "12312512",
+          update: [
+            {
+              startDate: "2023-09-01",
+              endDate: "2022-09-01",
+              carrier: "Some carrier",
+              id: "1251251",
+            },
+          ],
+          create: [
+            {
+              startDate: "2023-09-01",
+              endDate: "2022-09-01",
+              carrier: "Some carrier",
+            },
+          ],
+          delete: ["51254123125"],
+        },
+      },
+    });
+  }, [updateLossHistoryEntries]);
+
+  return (
+    <>
+      <h1>Loss History Entries</h1>
+      {loading || mutationLoading ? (
+        "loading"
+      ) : (
+        <code>{JSON.stringify(data, undefined, 2)}</code>
+      )}
+
+      <code>{error && JSON.stringify(error, undefined, 2)}</code>
+
+      <button onClick={mutateHandler}>Mutate</button>
+    </>
+  );
+}
