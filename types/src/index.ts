2afc45b177b0db80641ec8f55ab79e3148e649b6
davidrhoderick <david.e.rhoderick@gmail.com>
2024-11-22 09:13:47 +0100

    Adding official IDs types

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,1 +1,2 @@
-export * from './loss-history-entries'
+export * from "./loss-history-entries";
+export * from "./official-ids";
---
+++ types/src/official-ids.ts
@@ -0,0 +1,30 @@
+export interface IOfficialId {
+  label: string;
+  code: string;
+  id: string;
+}
+
+export interface IStateOfficialIds {
+  state: string;
+  displayName: string;
+  officialIds: Array<IOfficialId>;
+}
+
+export interface IStatesOfficialIds extends Array<IStateOfficialIds> {}
+
+export interface IListStatesOfficialIds {
+  listStatesOfficialIds: IStatesOfficialIds;
+}
+
+export interface IListStatesOfficialIdsArgs {
+  transactionId: string;
+}
+
+export interface IUpdateStatesOfficialIdsInput {
+  transactionId: string;
+  states: IStatesOfficialIds;
+}
+
+export interface IUpdateStatesOfficialIdsInputVariables {
+  updateStatesOfficialIdsInput: IUpdateStatesOfficialIdsInput;
+}
